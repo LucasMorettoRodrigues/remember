c77141712fc140ad5618426a3b9043c0291b5e95
Lucas <r.lucasmoretto@gmail.com>
2022-02-07 23:22:42 -0300

    style navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,20 +1,23 @@
 import { Link } from 'react-router-dom'
+import styles from './Navbar.module.css'
 
 const Navbar = () => {
     return (
-        <nav>
-            <Link to='/'>
-                <h1>Remember</h1>
-            </Link>
-            <ul>
-                <li>
-                    <Link to='/login'>Login</Link>
-                </li>
-                <li>
-                    <Link to='/register'>Register</Link>
-                </li>
-            </ul>
-        </nav>
+        <nav className={styles.navbar}>
+            <div className={styles.navbar_container}>
+                <Link to='/'>
+                    <h1>Remember</h1>
+                </Link>
+                <ul>
+                    <li>
+                        <Link to='/login'>Login</Link>
+                    </li>
+                    <li>
+                        <Link to='/register'>Register</Link>
+                    </li>
+                </ul>
+            </div >
+        </nav>    
     )
 }
 
