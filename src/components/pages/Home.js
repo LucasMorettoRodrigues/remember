35f1bbe2b21c0d4d92c28bb6ca70014d878d4a4b
Lucas <r.lucasmoretto@gmail.com>
2022-02-08 21:50:20 -0300

    develop home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,18 +1,19 @@
 import styles from './Home.module.css'
 import homeImg from '../../img/checklist.png'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
-        <main>
-            <div className={styles.row}>
-                <div className={styles.col_2}>
-                    <h1>Organize Your Life Now!</h1> < br/>
-                    <p>Manage your projects and tasks from anywhere.</p>
+        <main className={styles.row}>
+            <div className={styles.col_2}>
+                <h1>Organize Your Life Now!</h1> < br/>
+                <p>Manage your projects and tasks from anywhere.</p>
+                <Link to='/register'>
                     <button>Sing up now !</button>
-                </div>
-                <div className={styles.col_2}>
-                    <img src={homeImg} alt="Checklist"/>
-                </div>
+                </Link>
+            </div>
+            <div className={styles.col_2}>
+                <img src={homeImg} alt="Checklist"/>
             </div>
         </main>
     )
