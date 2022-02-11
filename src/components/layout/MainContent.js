b80bf1f2d5424cd27dc237b389dcc6c6d35ea466
Lucas <r.lucasmoretto@gmail.com>
2022-02-11 02:14:54 -0300

    develop new task form

--- src/components/layout/MainContent.js
+++ src/components/layout/MainContent.js
@@ -1,5 +1,6 @@
 import styles from './MainContent.module.css'
 import TaskCard from '../tasks/TaskCard'
+import Input from '../form/Input'
 
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
@@ -11,7 +12,10 @@ const MainContent = () => {
     const { id } = useParams() //Pega id vindo da URL 
 
     const [tasks, setTasks] = useState()
+    const [newTaskForm, setNewTaskForm] = useState(false)
     const [loading, setLoading] = useState(true)
+    const [taskName, setTaskName] = useState("")
+    const [taskDate, setTaskDate] = useState("")
 
     useEffect(() => {
         (async () => {
@@ -34,13 +38,29 @@ const MainContent = () => {
         id ? setTasks(resp.filter((resp) => resp.project === id )) : setTasks( resp )
     }
 
+    const createTask = (e) => {
+        e.preventDefault()
+        console.log(`Create ${taskName}, date: ${taskDate}`);
+        setNewTaskForm(false)
+    }
+
     return(
         <section className={styles.main_content}>
             <div className={styles.top_container}>
                 <h2>Project Name</h2>   
-                <button> Rename Project</button> 
-                <button> New Task</button> 
+                <button onClick={() => setNewTaskForm(true)}> New Task </button>
             </div>
+            {newTaskForm && 
+                <div>
+                    <form onSubmit={createTask}>
+                        <h2>New Task</h2>
+                        <Input id="task" text="Task" onChange={(e) => setTaskName(e.target.value)}/>
+                        <input type="date" onChange={(e) => setTaskDate(e.target.value)}/>
+                        <button type="submit">Create Task</button>
+                        <button onClick={() => setNewTaskForm(false)}>Close</button>
+                    </form>
+                </div>
+            }
             <ul>
                 {tasks.map((task) => (
                     <TaskCard handleDelete={handleDelete} key={task._id} taskId={task._id} task={task.task} date={task.date}/>
