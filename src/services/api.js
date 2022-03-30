b18daaf32f88954cf24f9b2da82c4336ca885920
Lucas <r.lucasmoretto@gmail.com>
2022-03-30 14:07:28 -0300

    edit api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://js-remember-api.herokuapp.com/'
 })
 
 export const createSession = async (name, password) => {
@@ -9,35 +9,35 @@ export const createSession = async (name, password) => {
 }
 
 export const registerUser = async (name, password) => {
-    return api.post('/api/v1/auth/register', {name, password})
+    return api.post('/api/v1/auth/register', { name, password })
 }
 
 export const getProjects = async => {
     return api.get('/api/v1/projects')
 }
 
-export const createProject = async (name)=> {
+export const createProject = async (name) => {
     return api.post(`/api/v1/projects/`, { name })
 }
 
-export const deleteProject = async (id)=> {
+export const deleteProject = async (id) => {
     return api.delete(`/api/v1/projects/${id}`)
 }
 
 export const getAllTasks = async (id) => {
-    return id 
-        ? api.get(`/api/v1/project/tasks/?project=${id}`) 
-        : api.get(`/api/v1/project/tasks`) 
+    return id
+        ? api.get(`/api/v1/project/tasks/?project=${id}`)
+        : api.get(`/api/v1/project/tasks`)
 }
 
 export const createTask = async (id, task, date) => {
     return api.post(`/api/v1/project/${id}/tasks`, { task, date })
 }
 
-export const updateTask = async (taskId, completed)=> {
+export const updateTask = async (taskId, completed) => {
     return api.patch(`/api/v1/project/tasks/${taskId}`, { completed })
 }
 
-export const deleteTask = async (taskId)=> {
+export const deleteTask = async (taskId) => {
     return api.delete(`/api/v1/project/tasks/${taskId}`)
 }
